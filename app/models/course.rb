7d06a13f7a1f040ab8941d87519789d1dcb9fb58
Kumi Takenaka <kumireliy@gmail.com>
2019-04-05 23:37:03 +0900

    working on tag

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord
   has_many :reviews
   belongs_to :school
+  acts_as_taggable_on :tags
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ApplicationRecord
   has_many :reviews
+  acts_as_taggable_on :tags
 end
