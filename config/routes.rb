875ea15719daa8f636bbc8687a64178a19714d7a
Kumi Takenaka <kumireliy@gmail.com>
2019-04-11 12:29:11 +0900

    still working on reviews_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root to: 'pages#home'
+
   get '/tagged', to: "schools#tagged", as: :tagged
 
   resources :schools, only: [:index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post '/reviews', to: 'reviews#create'
+
 end
