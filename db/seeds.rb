7c61ae8044d634b52c4e793af5c227240c7f1811
Kumi Takenaka <kumireliy@gmail.com>
2019-04-11 10:03:49 +0900

    10 seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,67 @@ puts 'photo uploaded!'
 school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554451460/ai-artificial-intelligence-codes-247791.jpg'
 school.save!
 
-puts "Added Just the two of us, Grover Washington song."
+
+school = School.new(name: 'TECH::EXPERT',
+          about: '600時間の学習時間によってプロのスキルを身につけ、キャリアアップを支えるプログラミングスクールです。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943643/techexpert.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943645/techexpert-banner.png'
+school.save!
+
+school = School.new(name: 'TechAcademy',
+          about: 'TechAcademyは、オンライン型のプログラミングスクールです。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943643/techacademy_logo.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943643/techadacemy-banner.jpg'
+school.save!
+
+school = School.new(name: 'WebCamp',
+          about: 'WebCampは、Webアプリ、デザインの指導を行なっているプログラミングスクールです。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943642/codecamp-logo.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943643/CodeCamp-banner.png'
+school.save!
+
+school = School.new(name: '０円スクール',
+          about: '0円スクールは、入学金、受講料、教材費など一切かからずにプログラミングを学ぶことができる画期的なスクールです。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943642/%EF%BC%90%E5%86%86%E3%82%B9%E3%82%AF%E3%83%BC%E3%83%AB-logo.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943642/%EF%BC%90%E5%86%86%E3%82%B9%E3%82%AF%E3%83%BC%E3%83%AB-banner.jpg'
+school.save!
+
+school = School.new(name: 'CodeCamp-banner',
+          about: 'CodeCampは、オンラインのプログラミング学習サービスです。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943642/codecamp-logo.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943643/CodeCamp-banner.png'
+school.save!
+
+school = School.new(name: 'POTEPAN CAMP',
+          about: 'POTEPAN CAMP（ポテパンキャンプ）は、「エンジニア専門転職支援のポテパン」と「急成長スタートアップCTO」のタッグで贈る、仕事獲得に特化したエンジニアスクールです。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943643/potepan-logo.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943644/%E3%81%BB%E3%82%9A%E3%81%A6%E3%83%8F%E3%82%9A%E3%83%B3-banner.jpg'
+school.save!
+
+school = School.new(name: 'KENスクール',
+          about: 'KENスクールは開校28年を迎え、未経験者をプロに育てる最適な学習形式を追及している学校です。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943642/kenschool-logo.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943643/kenschool-banner.jpg'
+school.save!
+
+school = School.new(name: 'ヒューマンアカデミー',
+          about: 'ヒューマンアカデミーの魅力は30年の伝統と述べ32万名以上の修了生を輩出している実績です。長年かけて蓄積されたデータとノウハウによって質の高い教育やサービスを提供しています。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943642/humanacademy-logo.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943644/%E3%83%92%E3%83%A5%E3%83%BC%E3%83%9E%E3%83%B3%E3%82%A2%E3%82%AB%E3%83%86%E3%82%99%E3%83%9F%E3%83%BC-banner.jpg'
+school.save!
+
+school = School.new(name: 'DIVE INTO CODE',
+          about: 'DIVE INTO CODE（ダイブイントゥーコード）は、Ruby on Railsを活用したWebアプリケーション開発に必要な知識を指導し、自力でプロトタイプを作成できるスキルをつけることを目標とするスクールです。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943642/diveintocode-logo.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943642/diveintocode-banner.jpg'
+school.save!
+
+school = School.new(name: 'WEB塾',
+          about: 'WEB塾はWEB制作会社の運営を通して得た、現状のWebスクールにはない現場の技術を伝えるために開設された学校です。')
+school.logo = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943644/%E3%82%A6%E3%82%A7%E3%83%95%E3%82%99%E5%A1%BE-logo.png'
+school.banner = 'https://res.cloudinary.com/drsb1n3km/image/upload/v1554943644/%E3%82%A6%E3%82%A7%E3%83%95%E3%82%99%E5%A1%BE-banner.jpg'
+school.save!
+
+
+
+puts "seeding done."
