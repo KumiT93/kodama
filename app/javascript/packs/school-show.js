9bb7acbbb86b17f26fd6d4789ea29e7bdc120534
Kumi Takenaka <kumireliy@gmail.com>
2019-04-18 12:02:58 +0900

    changed js

--- app/javascript/packs/school-show.js
+++ app/javascript/packs/school-show.js
@@ -1,20 +1,29 @@
 document.querySelector("#s-show-review").addEventListener("click", function() {
                   console.log("clicked");
                   document.querySelector('#review').style.display = "block";
+                  document.querySelector('.review').style.backgroundColor = "#F9F9F9";
                   document.querySelector('#course').style.display = "none";
+                  document.querySelector('.courses').style.backgroundColor = "white";
                   document.querySelector('#about').style.display = "none";
+                  document.querySelector('.about').style.backgroundColor = "white";
                   console.log("hola");
                 })
 document.querySelector("#s-show-course").addEventListener("click", function() {
                   console.log("clicked");
                   document.querySelector('#review').style.display = "none";
+                  document.querySelector('.review').style.backgroundColor = "white";
                   document.querySelector('#course').style.display = "block";
+                  document.querySelector('.courses').style.backgroundColor = "#F9F9F9";
                   document.querySelector('#about').style.display = "none";
+                  document.querySelector('.about').style.backgroundColor = "white";
                 })
 document.querySelector("#s-show-about").addEventListener("click", function() {
                   document.querySelector('#review').style.display = "none";
+                  document.querySelector('.review').style.backgroundColor = "white";
                   document.querySelector('#course').style.display = "none";
+                  document.querySelector('.courses').style.backgroundColor = "white";
                   document.querySelector('#about').style.display = "block";
+                  document.querySelector('.about').style.backgroundColor = "#F9F9F9";
                   console.log("clicked");
                 })
 
